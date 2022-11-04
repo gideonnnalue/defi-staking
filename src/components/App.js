f4826fb6b35fef3c12ca2aee82449bb29054eefb
Gideon Nnalue <emekannalue@gmail.com>
2022-11-04 20:32:27 +0100

    ui built

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,150 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Web3 from "web3";
+import Tether from "../truffle_abis/Tether.json";
+import RWD from "../truffle_abis/RWD.json";
+import DecentralBank from "../truffle_abis/DecentralBank.json";
+import ParticleSettings from "./ParticleSettings";
+import Main from "./Main";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      account: "0x0",
+      tether: {},
+      rwd: {},
+      decentralBank: {},
+      tetherBalance: "0",
+      rwdBalance: "0",
+      stakingBalance: "0",
+      networkId: 0,
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("No ethereum browser detected! You can check out MetaMask!");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    this.setState({ account: accounts[0], networkId });
+    // Load Contracts
+    await this.loadTetherData(web3);
+    await this.loadRwd(web3);
+    await this.loadDecentralBankData(web3);
+    this.setState({ loading: false });
+  }
+
+  async loadTetherData(web3) {
+    const tetherData = Tether.networks[this.state.networkId];
+    if (tetherData) {
+      const tether = new web3.eth.Contract(Tether.abi, tetherData.address);
+      this.setState({ tether });
+      let tetherBalance = await tether.methods
+        .balanceOf(this.state.account)
+        .call();
+      this.setState({ tetherBalance: tetherBalance.toString() });
+      console.log({ balance: tetherBalance });
+    } else {
+      window.alert(
+        "Error! Tether contract not deployed - no detected network!"
+      );
+    }
+  }
+
+  async loadRwd(web3) {
+    const rwdData = RWD.networks[this.state.networkId];
+    if (rwdData) {
+      const rwd = new web3.eth.Contract(RWD.abi, rwdData.address);
+      this.setState({ rwd });
+      let rwdBalance = await rwd.methods.balanceOf(this.state.account).call();
+      this.setState({ rwdBalance: rwdBalance.toString() });
+      console.log({ rwdbalance: rwdBalance });
+    } else {
+      window.alert("Error! RWD contract not deployed - no detected network!");
+    }
+  }
+
+  async loadDecentralBankData(web3) {
+    const decentralBankData = DecentralBank.networks[this.state.networkId];
+    if (decentralBankData) {
+      const decentralBank = new web3.eth.Contract(
+        DecentralBank.abi,
+        decentralBankData.address
+      );
+      this.setState({ decentralBank });
+      let stakingBalance = await decentralBank.methods
+        .stakingBalance(this.state.account)
+        .call();
+      this.setState({ stakingBalance: stakingBalance.toString() });
+      console.log({ stakingBalance });
+    } else {
+      window.alert(
+        "Error! Decentral Bank contract not deployed - no detected network!"
+      );
+    }
+  }
+
   render() {
-    return <div>Heloldll</div>
+    let content;
+
+    this.state.loading
+      ? (content = (
+          <p
+            id="loader"
+            className="text-center"
+            style={{ color: "white", margin: "30px" }}
+          >
+            LOADING PLEASE...
+          </p>
+        ))
+      : (content = (
+          <Main
+            tetherBalance={this.state.tetherBalance}
+            rwdBalance={this.state.rwdBalance}
+            stakingBalance={this.state.stakingBalance}
+            // stakeTokens={this.stakeTokens}
+            // unstakeTokens={this.unstakeTokens}
+            // decentralBankContract={this.decentralBank}
+          />
+        ));
+
+    return (
+      <div className="App" style={{ position: "relative" }}>
+        <div style={{ position: "absolute" }}>
+          <ParticleSettings />
+        </div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main
+              role="main"
+              className="col-lg-12 ml-auto mr-auto"
+              style={{ maxWidth: "600px", minHeight: "100vm" }}
+            >
+              <div>{content}</div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
